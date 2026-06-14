29a239af2903c98a330a0556ed30b85ce61d0795
ANJANA <anjanapradeesh7@gmail.com>
2026-06-14 22:15:53 +0530

    Remove secrets file from tracking

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,4 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = "https://qyvwpdcudifkyjfmgbrp.supabase.co";
-  static const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5dndwZGN1ZGlma3lqZm1nYnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MTUxNDEsImV4cCI6MjA5NjQ5MTE0MX0.IeA7dP0gKI1Uh8TU8QdW_jjYWZzGof0gpKgMFHR3hic";
-}
